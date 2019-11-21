2f3daddba8407ee9533bbbefcc2a4d6b8341bca2
Ryanet11 <49400375+Ryanet11@users.noreply.github.com>
2019-11-21 08:59:42 +0100

    Update form.php

--- php/form.php
+++ php/form.php
@@ -1,12 +1,8 @@
 <?php
     $email = $_POST['email_address'];
-    function filter_email($form_field) {
-        return preg_replace('/[\0\n\r\|\!\/\<\>\^\$\%\*\&]+/','',$form_field);
-    }
     $message = "test";
     $message = wordwrap($message, 70);
-    $sent = mail($email, 'Taakverdeling', $message, 'From: ryanjvanes@gmail.com\n');
-    if ($sent) {
+    if (mail($email, 'Taakverdeling', $message, 'From: automail@localhost')) {
         echo "<h1>Check je mail voor de taak verdeling.</h1>";
     } else {
         echo "<h1>Error.</h1>";
