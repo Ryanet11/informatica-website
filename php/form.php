a197a4f497aff7ee133502fde46a832238cf4563
Ryanet11 <49400375+Ryanet11@users.noreply.github.com>
2019-11-21 10:27:17 +0100

    upload pear

--- php/form.php
+++ php/form.php
@@ -1,10 +1,31 @@
 <?php
-    $email = $_POST['email_address'];
-    $message = "test";
-    $message = wordwrap($message, 70);
-    if (mail($email, 'Taakverdeling', $message, 'From: automail@localhost')) {
-        echo "<h1>Check je mail voor de taak verdeling.</h1>";
-    } else {
-        echo "<h1>Error.</h1>";
-    }
+require_once "Mail.php";
+
+$from = '<fromaddress@gmail.com>';
+$to = $_POST['email_address'];
+$subject = 'Taakverdeling';
+$body = "Hallo,\n\nDit is onze taak verdeling.";
+
+$headers = array(
+    'From' => $from,
+    'To' => $to,
+    'Subject' => $subject
+);
+
+$smtp = Mail::factory('smtp', array(
+        'host' => 'ssl://smtp.gmail.com',
+        'port' => '465',
+        'auth' => true,
+        'username' => 'ryan.j.van.es@gmail.com',
+        'password' => 'krs4pkrs4p'
+    ));
+
+$mail = $smtp->send($to, $headers, $body);
+
+if (PEAR::isError($mail)) {
+    echo('<p>' . $mail->getMessage() . '</p>');
+} else {
+    echo('<p>Message successfully sent!</p>');
+    echo('<p>to: '.$_POST['email_address'].'</p>');
+}
 ?>
